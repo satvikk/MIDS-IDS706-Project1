90a7b8b993201ea486f16d230122f0cb48befa9d
EC2 Default User <ec2-user@ip-172-31-64-135.ec2.internal>
2021-08-30 01:07:10 +0000

    added app

--- app.py
+++ app.py
@@ -5,3 +5,9 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+    
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production app.
+    app.debug = True
+    app.run()
