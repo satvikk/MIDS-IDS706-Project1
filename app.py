a297ec77c8a3089e38fac404ac9366e262237ff6
EC2 Default User <ec2-user@ip-172-31-79-30.ec2.internal>
2021-09-01 18:11:19 +0000

    made birth thingy

--- app.py
+++ app.py
@@ -7,18 +7,21 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     """Return a friendly HTTP greeting."""
-    print("I am inside hello world")
-    return "Continuous Delivery Demo"
+    print("Inside Root")
+    return "<p> Append your birth month number to the url (with a slash) </p>"
 
 
-@application.route("/echo/<name>")
-def echo(name):
-    print(f"This was placed in the url: new-{name}")
-    val = {"new-name": name}
-    return jsonify(val)
+@application.route("/echo/<m>")
+def echo(m):
+    print(f"This was placed in the url: {m}")
+    if m not in record.keys:
+        return {"error":"invalid"}
+    record[m] += 1
+    return {m:record[m]}
 
 
 if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
+    record = {i:0 for i in range(1,13)}
     application.run(host="0.0.0.0", port=8080, debug=True)
