f0ed4cb5ba2552c4b80a2803f9c18cca4b89596e
EC2 Default User <ec2-user@ip-172-31-79-30.ec2.internal>
2021-09-04 01:27:38 +0000

    prepping for deployment

--- test_app.py
+++ test_app.py
@@ -1,11 +1,11 @@
 import app
 
 def test_hello():
-	a = app.hello()
+    a = app.hello()
 
-#def test_echo():
-#    record = {i:0 for i in range(1,13)}
-#    a = echo(2)
-#    assert a[2] == "February"
-#    b = echo(13)
-#    assert b["error"] == "invalid input"
+def test_echo():
+    record = {i:0 for i in range(1,13)}
+    a = app.echo(2)
+    assert a[2] == "February"
+    b = app.echo(13)
+    assert b["error"] == "invalid input"
