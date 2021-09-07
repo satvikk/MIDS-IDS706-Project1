070260b17d0afadc195b8d6653d5c7e727391d03
EC2 Default User <ec2-user@ip-172-31-79-30.ec2.internal>
2021-09-07 20:50:04 +0000

    typo fix

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def hello():
 @application.route("/<m>")
 def echo(m):
     if m not in [str(i) for i in range(1,13)]:
-        return {"error": "Invlaid input"}
+        return {"error": "invalid input"}
     m = int(m)
     record = {
         1:"January", 2:"February", 3:"March", 4:"April", 5:"May", 6:"June", 7:"July", 8:"August", 9:"September", 10:"October", 
